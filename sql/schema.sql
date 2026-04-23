1267d59f197da7de5a902d8dca722e7831ff90f0
Antonin GENELOT <jaimelesfraisesbleues@gmail.com>
2026-04-23 21:21:38 +0200

    add database schema

---
+++ sql/schema.sql
@@ -0,0 +1,22 @@
+
+CREATE TABLE vault (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    salt VARCHAR(24) NOT NULL, -- base64(16 bytes) = 24 bytes
+    created_at TIMESTAMP DEFAULT NOW(),
+);
+
+CREATE TABLE vault_entry (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    ciphertext VARCHAR(255) NOT NULL,
+    nonce VARCHAR(255) NOT NULL,
+    
+    -- user fields
+    website VARCHAR(255) NOT NULL,
+    label VARCHAR(255),
+    
+    -- audit
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP,
+
+    vault_id UUID NOT NULL REFERENCES vault(id) ON DELETE CASCADE,
+);
